77b2fddff7154d540c5b28b4ea132ad3f69bcb26
ChloeChepigin <chloe.chepigin@yale.edu>
2018-01-28 14:25:17 -0500

    Initial commit

---
+++ usingQuestion1.py
@@ -0,0 +1,5 @@
+import Question1 as eq
+# using the information from Question1.py to get a health score with the example from the webpage listed in the HW2 pdf
+
+print ("Score for", 2, 1, 1, 2, 1, 2, 1, 3 )
+print(eq.get_score(2, 1, 1, 2, 1, 2, 1, 3))
